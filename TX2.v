7ca6b1de54465562e419916f7db8ed1da32c7e6c
tappleseth <tomappleseth@gmail.com>
2015-03-08 18:48:51 -0700

    Update TX2.v
    The latest TX

--- TX2.v
+++ TX2.v
@@ -1,11 +1,11 @@
-module TX(DataOut, DataIn, Pload, enable, charReceived, CLOCK_50, reset);
+module TX(DataOut, DataIn, Pload, Enable, charReceived, CLOCK_50, reset);
 
 output reg DataOut;
 output charReceived;
 input [7:0] DataIn;
-input Pload, enable, CLOCK_50, reset;
+input Pload, Enable, CLOCK_50, reset;
 
-wire Enable;
+//wire Enable;
 wire Clock16;
 wire count;
 wire shift;
@@ -14,13 +14,13 @@ wire kill;
 initial DataOut = 1;
 //assign Enable = ~charReceived;
 
-ShiftRegisterOut bro1 (shiftedData, DataIn, Pload, Enable, shift, reset);
-BSC bro2 (count, shift, Clock16, Enable, reset);
-BIC bro3 (charReceived, count, Enable, Clock16, kill, reset);
-clock16 bro4 (Clock16, CLOCK_50, reset);
-charRecKiller omgWhy(kill, charReceived, Clock16, reset);
+ShiftRegisterOut TXSFR(shiftedData, DataIn, Pload, Enable, shift, reset);
+BSC txBSC(count, shift, Clock16, Enable, reset);
+BIC txBIC(charReceived, count, Enable, Clock16, kill, reset);
+clock16 txClock16 (Clock16, CLOCK_50, reset);
+charRecKiller txClear(kill, charReceived, Clock16, reset);
 
-CuteTestingModule stoatenshire(Enable, Pload, charReceived);
+//CuteTestingModule stoatenshire(Enable, Pload, charReceived);
 
 always @(posedge CLOCK_50 or posedge reset) begin
 	if(reset) DataOut = 1;
@@ -30,7 +30,7 @@ end
 
 endmodule
 
-module CuteTestingModule(enable, Pload, charReceived);
+/*module CuteTestingModule(enable, Pload, charReceived);
 input Pload, charReceived;
 output reg enable;
 initial enable = 0;
@@ -42,7 +42,7 @@ always @(posedge charReceived) begin
 	enable = 0;
 	end
 
-endmodule
+endmodule*/
 
 module ShiftRegisterOut(OutData, InData, Pload,enable, clk, reset);
 
@@ -72,113 +72,18 @@ else OutData = 1;
 end*/
 
 endmodule
-
 module D_FF_P(q, d, pl, parallelLoad, reset, clk); 
  output q; 
  input d, reset, clk;
  input pl, parallelLoad;
  
  reg q; // Indicate that q is stateholding 
- 
- always @(posedge clk or posedge reset) 
+ initial q = 0;
+ always @(posedge clk or posedge reset or negedge parallelLoad) 
  if (reset) 
  q = 0; // On reset, set to 0 
  else begin
- 
- if (!parallelLoad) q = d; // Otherwise out = d 
- else q = pl;
- 
+ if (!parallelLoad) q = pl; // Otherwise out = d 
+ else q = d;
  end
-endmodule
-
-module BSC(count, clk_ctrl, clk, enable, reset);
-
-//For the BSC, state 0000 indicates the start of a bit, 0111 indicates the middle, and 1111 indicates the end of the bit. 
-
-output reg count;
-output reg clk_ctrl;
-input enable, clk, reset;
-reg[3:0] counter;
-initial counter = 0;
-
-always@ (posedge clk or posedge reset or negedge enable) begin
-	if (reset) begin counter = 0; count = 0; end	
-	else if(!enable) begin counter = 0; count = 0; end
-	else begin 
-	case(counter) 
-	4'b0111: clk_ctrl = 1;
-	4'b1111: count = 1;
-	default: begin count = 0; clk_ctrl = 0; end
-	endcase
-	counter = counter + 1;
-	end
-	end
-
-endmodule
-
-module BIC(charRec, BSCstate, enable, clock16, charDisable, reset);
-
-	output reg charRec;
-	input clock16;
-	input BSCstate;
-	input enable;
-	input reset;
-	input charDisable;
-	
-	reg[3:0] counter;
-	initial counter = 4'b0;
-	initial charRec = 0;
-	
-	always @(posedge clock16) begin
-		if (counter == 9) charRec = 1;
-		if (charDisable) charRec = 0;
-	end
-	//assign charRec = (counter == 9) ? 1 : 0;
-	always @(posedge BSCstate or posedge reset or negedge enable) 
-	begin
-		if (reset) counter = 0;
-		else case(enable)
-			1: counter = counter + 1;
-			0: counter = 0;
-		endcase
-		
-	end
-	
-endmodule
-
-module clock16(slowClock, CLOCK_50, reset);
-	input CLOCK_50;
-	input reset;
-	output reg slowClock;
-	initial slowClock = 0;
-
-	reg[8:0] clockFaker;
-	initial clockFaker = 0; //count to 326
-
-	always @(posedge CLOCK_50 or posedge reset) begin
-		if (reset) clockFaker = 0;
-		else begin 
-			if (clockFaker == 300) begin clockFaker = 0; slowClock = 1; end
-			else begin clockFaker = clockFaker + 1; slowClock = 0; end
-		end
-	end
-endmodule
-
-module charRecKiller(kill, charReceived, Clock16, reset);
-
-output reg kill;
-input charReceived, Clock16, reset;
-reg[3:0] counter;
-initial counter = 0;
-
-always @(posedge Clock16 or posedge reset) begin
-
-	if(reset) counter = 0;
-	else if(charReceived) case(counter)
-	7: begin kill = 1; counter = 0; end
-	default: begin kill = 0; counter = counter + 1; end
-	endcase
-	else begin counter = 0;	kill = 0; end
-	end
-	
 endmodule
